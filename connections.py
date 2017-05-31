902d848db97534189d4fc40375c11f6aeae04eec
Juliano Naves <julianofischer@gmail.com>
2017-05-31 19:51:14 -0400

    últimas modificações - funcionando

--- connections.py
+++ connections.py
@@ -9,6 +9,9 @@ class Event:
         self.to_node = d["to"]
         self.time = d["time"]
         self.__status = d["status"]
+        self.opening = None
+
+        #print("Constructing event with status %s" % (self.__status,))
 
         if self.__status == "up":
             self.opening = True
@@ -20,6 +23,9 @@ class Event:
     def is_opening(self):
         return self.opening
 
+    def __str__(self):
+        return "event [%d,%d,%d,%s]" % (self.time, self.from_node, self.to_node, self.__status)
+
 
 class Connection:
     def __init__(self, event):
@@ -29,28 +35,32 @@ class Connection:
         if event:
             self.from_node = event.from_node
             self.to_node = event.to_node
-            self.init_time = event.init_time
+            self.init_time = event.time
 
     def duration(self):
         assert self.end_time, "end_time not set - unable to compute duration"
-        return self.end_time - self.init_tim
+        return self.end_time - self.init_time
 
     def get_id(self):
         return str(min(self.from_node, self.to_node)) + ":"+str(max(self.from_node,self.to_node))
 
     # Se a conexão já foi fechada
     def was_closed(self):
-        return self.end_time != None
+        return self.end_time is None
 
     # def __eq__(self, other):
     #    return isinstance(other,self.__class__) and self.from_node == other.from_node and self.to_node == other.to_node and self.init_time == other.init_time
 
-    def is_same_connections(self, c):
+    def is_same_connection(self, c):
         return (c.from_node == self.from_node and self.to_node == c.to_node) or (
-        c.from_node == self.to_node and c.to_node == self.from_node)
+            c.from_node == self.to_node and c.to_node == self.from_node)
 
         # def __hash__(self):
         #    if self.from_node > self.to_node:
         #        return hash((self.from_node, self.to_node))
         #    else:
         #        return hash((self.to_node,self.from_node))
+
+    def __str__(self):
+        end = self.end_time if self.end_time is not None else -1
+        return "connection [%d, %d, %d, %d]" % (self.init_time, self.from_node, self.to_node, end)
--- traceanalysis.py
+++ traceanalysis.py
@@ -6,6 +6,8 @@ import networkx as nx
 
 from connections import Connection, Event
 
+COMPONENTS_FILE_NAME = "components_log.txt"
+
 # extracting arguments
 parser = argparse.ArgumentParser(description="Descrição")
 parser.add_argument("-f","--file",dest="filename",help="the trace file that will be analyzed", metavar="FILENAME")
@@ -36,25 +38,26 @@ def _init():
     f = open(filename)
 
     # default value is 30
-    logging_step = args.log_step if args.log_step else 30
+    logging_step = int(args.log_step) if int(args.log_step) else 30
 
-    endtime = args.endtime
+    endtime = int(args.endtime)
 
     created_connections = []
     open_connections = []
     connected_components_log = []
     largest_connected_component = 0
+    last_log = 0
 
 
 # pega a linha do trace e retorna um objeto de Event
 def get_event(line):
     l = line.split()
     d = dict()
-    d["from"] = int(l[2]);
-    d["to"] = int(l[3]);
-    d["event"] = l[1];
-    d["time"] = int(l[0]);
-    d["status"] = l[4];
+    d["from"] = int(l[2])
+    d["to"] = int(l[3])
+    d["event"] = l[1]
+    d["time"] = int(l[0])
+    d["status"] = l[4]
     e = Event(d)
     return e
 
@@ -75,15 +78,19 @@ def apply_graph_change(event):
 
 # e:o evento a ser processado
 def process_event(e):
+    # print ("processing event: "+str(e))
     # cria ou remove uma aresta do grafo
     apply_graph_change(e)
 
     # adiciona uma conexão a open_connections ou encerra uma conexão
-    if e.is_opening:
+    if e.is_opening():
         c = Connection(e)
         open_connections.append(c)
+        created_connections.append(c)
+        # print ("Opening connection "+str(c))
     else:
         # é um evento que fecha a conexão
+        # print("Fecha conexão")
         close_connection(e)
 
     # faz outras coisas: salva relatório, mede alguma coisa, etc...
@@ -106,13 +113,25 @@ def close_connection(e):
 def get_events_at_instant(time):
     global last_file_position
     last_file_position = f.tell()
-    l = f.readline()
-    e = get_event(l)
+    l = f.readline().strip()
     _list = []
 
-    while e["time"] == time:
+    # end of the file
+    if len(l) == 0:
+        return _list
+
+    e = get_event(l)
+
+    while e.time == time:
         _list.append(e)
         last_file_position = f.tell()
+        l = f.readline().strip()
+
+        # end of the file
+        if len(l) == 0:
+            break
+
+        e = get_event(l)
 
     # volta o ponteiro do arquivo para a última linha que foi lida
     f.seek(last_file_position)
@@ -124,9 +143,12 @@ def get_events_at_instant(time):
 def run():
     global largest_connected_component, last_log
 
-    for instant in range(0,endtime):
+    for instant in range(0, endtime+1):
+        # print("Instant %d" % (instant,))
         events = get_events_at_instant(instant)
+
         for e in events:
+            # print (e)
             process_event(e)
 
         # get the largest_connected_component
@@ -134,18 +156,21 @@ def run():
         larger = max([len(x) for x in connected_components])
         largest_connected_component = larger if larger > largest_connected_component else largest_connected_component
 
+        list_of_connected_components = list(nx.connected_components(g))
+
         # instant is equivalent to 'now'
         if instant - last_log > logging_step:
             last_log = instant
-            connected_components_log.append("%d    %s" % (instant, str(connected_components)))
+            connected_components_log.append("%d    %s\n" % (instant, str(list_of_connected_components)))
 
+    close_remaining_connections(endtime)
     # perform post processing information extraction
     post_processing()
 
 
 # setting end time for connections which are not closed at trace ending
 def close_remaining_connections(t):
-    for c in created_connections:
+    for c in open_connections:
         c.end_time = t
 
 
@@ -153,16 +178,22 @@ def close_remaining_connections(t):
 def post_processing():
     # write down the log information
     print("Number of connections: %d" % (get_number_of_connections(),))
-    print("Average number of connections (per nodes): %d" % (get_average_number_of_connections(),))
-    print("Connections per minute: %d" % (get_connections_per_minute(),))
+    print("Average number of connections (per nodes): %f" % (get_average_number_of_connections(),))
+    print("Connections per minute: %f" % (get_connections_per_minute(),))
     print("Total connection time: %d" % (get_total_connection_time(),))
-    print("Average connection time (per connection): %d" % (get_average_connection_time()))
+    print("Average connection time (per connection): %f" % (get_average_connection_time()))
     print("Largest connected component: %d" % (largest_connected_component,))
     do_the_log()
 
 
 def do_the_log():
-    pass
+    # logging components
+    components_file = open(COMPONENTS_FILE_NAME, "w+")
+    components_file.writelines([str(c) for c in connected_components_log])
+
+    # loggin connections
+    connections_out_file = open("connections_out_file.txt", "w+")
+    connections_out_file.writelines([str(c)+"\n" for c in created_connections])
 
 
 def get_number_of_connections():
@@ -182,8 +213,9 @@ def get_average_connection_time():
 
 
 def get_connections_per_minute():
-    minutes = endtime / 60;
+    minutes = endtime / 60
     return get_number_of_connections() / minutes
+
 
 def main():
     _init()
